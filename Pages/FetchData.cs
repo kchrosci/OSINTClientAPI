74329719f679df9c238300e0cbb3912885d9ac3e
kchrosci <kamil.chroscicki.stud@pw.edu.pl>
2021-05-20 01:19:39 +0200

    Changes

---
+++ Pages/FetchData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using OSINTClientAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OSINTClientAPI.Pages
+{
+	public partial class FetchData
+	{	
+		[Inject]
+		private HttpClient Http { get; set; }  
+
+		private LaunchDto[] launches;
+
+		protected override async Task OnInitializedAsync()
+		{
+			launches = await Http.GetFromJsonAsync<LaunchDto[]>("/rest/launches/");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -17,9 +17,10 @@ namespace OSINTClientAPI
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["api_base_url"]) });
 
 			await builder.Build().RunAsync();
 		}
 	}
 }
+ 
